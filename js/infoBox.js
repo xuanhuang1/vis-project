5bbdd68bf33b66b66097ddda5e1e68859e315db3
Haihan Lin <haihanlin@HaihandeMacBook.local>
2018-11-30 14:25:40 -0700

    info update

--- js/infoBox.js
+++ js/infoBox.js
@@ -20,16 +20,16 @@ class InfoBox{
 
         this.svg.append('svg:image')
             .attr('xlink:href','https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Metacritic.svg/2000px-Metacritic.svg.png')
-            .attr('x',0)
-            .attr('y',40)
+            .attr('x',5)
+            .attr('y',30)
             .attr('width',20)
-            .attr('height',20)
+            .attr('height',20);
 
         this.svg.append('text')
             .attr('id','Metacritic')
             .classed('infoBox',true)
-            .attr('x',5)
-            .attr('y',40)
+            .attr('x',30)
+            .attr('y',45);
 
         this.svg.append('text')
             .attr('id','gameDescription')
@@ -76,7 +76,7 @@ class InfoBox{
     updateInfoBox(gameSelected){
         this.svg.select('#background').attr('xlink:href',gameSelected.Background);
         this.svg.select('#gameTitle').text('Title: ' + gameSelected.QueryName);
-        this.svg.select('#Metacritic').text('MetaCritic')
+        this.svg.select('#Metacritic').text(gameSelected.Metacritic==='0'?' : Not Available' : (' : '+ gameSelected.Metacritic));
         this.svg.select('#gameDescription').text(gameSelected.AboutText)
             .call(this.wrap,this.width-10)
             .attr('overflow','scroll')
